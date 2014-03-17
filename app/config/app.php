13529615b3c275b8a07008c7b46fa50123ec483d
jgallred <jgallred86@gmail.com>
2014-03-17 06:37:21 -0600

    Removing vagrant directory

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'q36IDsUVtvsZvNQz6rAuzSkWwHjbsR3U',
 
 	/*
 	|--------------------------------------------------------------------------
